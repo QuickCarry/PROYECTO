eab765251ec06cd78ff80e31a5f8221b7268cdb5
QuickCarry <quickcarry830@gmail.com>
2023-11-15 20:36:14 -0300

    Actualice los códigos

--- CODIGO S.O/mysql-backup.sh
+++
@@ -1,7 +0,0 @@
-#/vim/bash
-
-fecha=$(date +%Y%m%d)
-
-mkdir -p "/opt/mysql/mysqlbackup/respaldo$fecha" #Crea la carpeta donde se almacenara el respaldo si no exite con la fecha al final.
-
-tar -cvf "/opt/mysql/mysqlbackup/respaldo$fecha/respaldoBD" "/opt/mysql/SinerGync" #empaqueta la base de datos "SinerGync" y redirige a "/opt/mysql/mysqlbackup/respaldo(fecha)/respaldoBD"
